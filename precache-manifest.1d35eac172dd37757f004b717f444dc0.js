c781e5571db22ede6f12bb223f928c992dd9d3be
James Mellish <jameswmellish@gmail.com>
2020-09-28 15:16:35 -0500

    Updates

---
+++ precache-manifest.1d35eac172dd37757f004b717f444dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67d988c6933333bfc07f12644567863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96eb54979b17d6ec3414",
+    "url": "/static/css/main.35c8ed64.chunk.css"
+  },
+  {
+    "revision": "f325a1d9c8eca212f2d8",
+    "url": "/static/js/2.c76f00d7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c76f00d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96eb54979b17d6ec3414",
+    "url": "/static/js/main.81da481a.chunk.js"
+  },
+  {
+    "revision": "057abab1acc7bec37270",
+    "url": "/static/js/runtime-main.cba1fa26.js"
+  }
+]);
